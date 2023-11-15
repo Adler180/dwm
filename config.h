dc09007ae9d016cefde44884476e4920a602b4d0
Adler180 <handt2003@gmail.com>
2023-11-15 22:16:03 +0100

    Zoom floating

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "zoom",     NULL,       NULL,       6,       	    1,           -1 },
 };
 
 /* layout(s) */
